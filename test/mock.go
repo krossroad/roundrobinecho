8a6c3ffb5907ecb1c88b50bf5b4f234ed5d294d1
Rikesh Shrestha <rikesh.shrestha.npl@gmail.com>
2024-09-04 11:30:42 +0700

    feat: Add necessary mocks

---
+++ test/mock.go
@@ -0,0 +1,4 @@
+package test
+
+//go:generate go run github.com/vektra/mockery/v2@v2.45.0 --case=snake  --dir=../internal/loadbalancer --name= --case=snake --all --output=mocks
+//go:generate go run github.com/vektra/mockery/v2@v2.45.0 --case=snake  --dir=../internal/echo --name= --case=snake --all --output=mocks
